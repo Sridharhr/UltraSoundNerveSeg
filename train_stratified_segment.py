930a5f881d1149ea255eb975e74a59bf4dedb0e0
Sridharhr <sridharhr@sridhars-mbp.in.ibm.com>
2017-07-04 12:31:22 +0530

    modified:   train_stratified_segment.py

--- train_stratified_segment.py
+++ train_stratified_segment.py
@@ -55,9 +55,9 @@ print('-'*30)
 
 # create model
 model = KerasClassifier(build_fn=get_unet, epochs=20, batch_size=32, verbose=1)
-epochs=[5,10,20,40,80]
-batches = [8, 16, 32, 64, 128]
-lr = [1e-3,1e-4,1e-5,1e-6,1e-7]
+epochs=[5,10,15,20]
+batches = [8, 16, 32, 64]
+lr = [1e-3,1e-4,1e-5,1e-6]
 param_grid = dict(epochs=epochs, batch_size=batches, lr_in=lr)
 #TODO: try out setting iid = False below
 grid = GridSearchCV(estimator=model, param_grid=param_grid)
